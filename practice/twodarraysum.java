db455b7729f9b53b3328006d026e2b3cfc4bd524
vanshikagupta242 <vanshikagupta8070@gmail.com>
2026-02-03 10:06:26 +0530

    twodarraysum

---
+++ practice/twodarraysum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class twodarraysum
+{
+public static void main (String [] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter rows");
+int r = sc.nextInt();
+System.out.println("Enter colums");
+int c = sc.nextInt();
+ int [][] arr  = new int [r][c];
+ for (int i = 0 ; i < r ; i++){
+ for(int j = 0 ; j < c ; j++){
+ arr [i][j] = sc.nextInt();
+ }
+ }
+ int sum = 0;
+ for (int i = 0 ; i < r ; i++){
+ for (int j = 0 ; j < c ; j++){
+ sum += arr[r][c];
+ }
+ }
+ System.out.println("sum of 2d arr" + sum);
+ }
+ }
