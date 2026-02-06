e6bb5e53b5b447db88bc8bb5f360147c2f9d9e10
vanshikagupta242 <vanshikagupta8070@gmail.com>
2026-02-06 11:57:57 +0530

    removeDuplicates

---
+++ practice/Strings.java
@@ -0,0 +1,19 @@
+class Strings {
+	public static void main(String args[])
+	{
+		String str = "hello world !!";
+		
+		//length
+		System.out.println(str.length);
+		
+		//touppercase()
+		System.out.println(str.toUppercase());
+		
+		//tolowercase()
+		System.out.println(str.toLowercase());
+		
+		// charAt()
+		System.out.println(str.charAt(0));
+	}
+}
+		
