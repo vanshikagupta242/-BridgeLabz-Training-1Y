16c3eb0549ef4921a5ad781a5311427c2928e6fd
vanshika_gupta <vanshikagupta8070@gmail.com>
2026-04-10 22:33:14 +0530

    save changes

--- BasicProblems/parctice program/Solution1.java
+++
@@ -1,9 +0,0 @@
-
-public class Solution1 {
-    public static void main(String[] args) {
-        // Assisted Problem 1: Welcome to Bridgelabz
-        // Write a program that prints "Welcome to Bridgelabz!" to the screen.
-
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
